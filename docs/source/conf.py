b606fbb3556cf20c2493046152f63860662d64ae
Jinyifei <yifei.jin.astro@gmail.com>
2024-12-15 21:36:20 -0500

    .

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -25,7 +25,7 @@ exclude_patterns = []
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_theme = 'sphinx_rtd_theme'
-html_static_path = ['_static']
+#html_static_path = ['_static']
 html_logo = 'm3logo6_profile.png'
 html_theme_options = {
      'logo_only': True
